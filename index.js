77b69ec2e2d73b1d401504f2271f3598538f384b
Robin Doirin <robindoirin@gmail.com>
2019-10-27 00:24:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+
 const{name, color, song, job, partner} = muppet;
 const{song_2, song_4} = k_muppet.k_album.the_muppet_movie;
 const{k_job, k_partner} = k_muppet;
